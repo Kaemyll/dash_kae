475baca6b004656baa885dfc6a978cc476cca01b
Garnier David <david.garnier483@orange.fr>
2023-05-05 13:04:25 +0200

    dev: new branch for stylizing Layout & install react-router-dom

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+
+import { Link } from "react-router-dom";
+
 import { Button, Container, Nav, Navbar, Form } from "react-bootstrap";
 import Evelogo from "../../assets/img/eve_logo.png";
 
 export default function Header() {
     return (
-        <Navbar bg="dark" variant="dark">
+        <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">
+                <Navbar.Brand href="/">
                     <img
                         alt="logo"
                         src={Evelogo}
@@ -17,6 +20,8 @@ export default function Header() {
                     KAE Dashboard
                 </Navbar.Brand>
                 <Nav className="mr-auto">
+                    <NavLink href="/">Home</NavLink>
+                    <NavLink href="/contact">Me contacter</NavLink>
                     <Nav.Link href="#addTask">Ajouter une tâche</Nav.Link>
                     <Nav.Link href="#editTask">Editer une tâche</Nav.Link>
                     <Nav.Link href="#deleteTask">Supprimer une tâche</Nav.Link>
