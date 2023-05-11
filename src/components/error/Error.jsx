dfcf418d6271472c406108630b3939c27f841b35
Garnier David <david.garnier483@orange.fr>
2023-05-11 10:40:38 +0200

    style: updating dependencies, adding icons

--- src/components/error/Error.jsx
+++ src/components/error/Error.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+import { TbError404 } from "react-icons/tb";
+
 export default function Error() {
     return (
         <div>
-            <h2>Cette page n'existe pas !</h2>
+            <h2 className="me-auto text-center bg-danger text-warning">
+                <TbError404 /> Cette page n'existe pas !<TbError404 />
+            </h2>
         </div>
     );
 }
--- src/components/pages/ManageTasks.jsx
+++ src/components/pages/ManageTasks.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import { Nav } from "react-bootstrap";
+import { MdAddTask, MdEditSquare, MdDeleteForever } from "react-icons/md";
 
 export default function ManageTask() {
     return (
@@ -9,12 +10,15 @@ export default function ManageTask() {
             <h2>Gestion des tâches</h2>
             <Nav className="me-auto">
                 <Nav.Link as={Link} to="/addTask">
+                    <MdAddTask />
                     Ajouter une tâche
                 </Nav.Link>
                 <Nav.Link as={Link} to="/editTask">
+                    <MdEditSquare />
                     Editer une tâche
                 </Nav.Link>
                 <Nav.Link as={Link} to="/deleteTask">
+                    <MdDeleteForever />
                     Supprimer une tâche
                 </Nav.Link>
             </Nav>
